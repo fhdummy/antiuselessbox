00d8aa4ad5f87d11449b8afee36259cc892f61c2
fhdummy <Stefan@Desktop.com>
2015-12-01 18:05:19 +0100

    Distance sensor is working - it display the timer value

--- antiuselessbox/main.c
+++ antiuselessbox/main.c
@@ -6,13 +6,70 @@
  */ 
 
 #include <avr/io.h>
+#include <util/delay.h>
 
+#define PING_PIN PA0
+#define PING_DDR DDRA
+#define PING_OUTPUT_PORT PORTA
+#define PING_INPUT_PORT PINA
+
+unsigned int distance = 0;
+
+void initPing()
+{
+}
+
+void executePing()
+{
+	PING_DDR |= (1 << PING_PIN);
+	PING_OUTPUT_PORT |= (1 << PING_PIN);	//Set pin high
+	_delay_ms(5);
+	PING_OUTPUT_PORT &= ~(1 << PING_PIN);	//Set pin low
+	PING_DDR &= ~(1 << PING_PIN);	//Set pin to input
+	
+	while(!(PING_INPUT_PORT & (1 << PING_PIN)))	//Wait until the pin is high
+	{
+		
+	}
+	startTimer1();
+	
+	while(PING_INPUT_PORT & (1 << PING_PIN))	//Wait until the pin is low again
+	{
+		
+	}
+	stopTimer1();
+	distance = TCNT1;
+	TCNT1 = 0;
+}
+
+void startTimer1()
+{
+	//Normal-Mode
+	TCCR1A &= ~((1 << WGM10) | (1 << WGM11));
+	TCCR1B &= ~((1 << WGM12) | (1 << WGM13));
+		
+	//Prescaler 64
+	TCCR1B |= ((1 << CS10) | (1 << CS11));
+	TCCR1B &= ~((1 << CS12));
+		
+	TCNT1 = 0;
+}
+
+void stopTimer1()
+{
+	TCCR1B &= ~((1 << CS10) | (1 << CS11) | (1 << CS12));	//Stop the timer
+	//TCNT1 = 0;	
+}
 
 int main(void)
 {
+	initPing();
+	
+	
     /* Replace with your application code */
     while (1) 
     {
+		executePing();
     }
 }
 
