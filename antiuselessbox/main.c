bef2f18c7cf7d297e2178de9e195a14e67a88ba8
fhdummy <Stefan@Desktop.com>
2015-12-01 17:27:24 +0100

    Initial commit - empty project

---
+++ antiuselessbox/main.c
@@ -0,0 +1,18 @@
+/*
+ * antiuselessbox.c
+ *
+ * Created: 01.12.2015 17:26:00
+ * Author : Stefan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
